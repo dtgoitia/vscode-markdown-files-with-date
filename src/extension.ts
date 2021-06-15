6a058788b5a2c8b786f6d01f5d73a487c8ed7bf9
David Torralba Goitia <david.torralba@babylonhealth.com>
2021-06-15 13:55:33 +0100

    POC

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,84 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as fs from 'fs';
+import * as path from 'path';
 import * as vscode from 'vscode';
+
+type AbsolutePath = string;  // Absolute path as string, e.g. "/bin/bash"
+
+function logError(msg: string): void {
+	console.error(msg)
+	vscode.window.showErrorMessage(msg)
+}
+
+function getIsoDate(): string {
+	const isoDate = new Date().toISOString().substring(0,10);
+	return isoDate;
+}
+
+function getFileName(): string {
+	const isoDate = getIsoDate();
+	const fileName = `${isoDate}.md`;
+	return fileName;
+}
+
+function createPathUnder(dir: AbsolutePath): AbsolutePath {
+	const fileName = getFileName();
+	const filePath = `${dir}/${fileName}`;
+	return filePath;
+}
+
+function createPathInSameDirectoryAs(file: AbsolutePath): AbsolutePath {
+	const dir = path.dirname(file);
+	const filePath = createPathUnder(dir);
+	return filePath;
+}
+
+class FileAlreadyExistsError extends Error { }
+
+function createFile(filePath: AbsolutePath): void {
+	const fileExists = fs.existsSync(filePath);
+
+	if (fileExists) {
+		throw new FileAlreadyExistsError(`File ${filePath} exists`);
+	}
+
+	const fileContent = "FETCH SNIPPET CONTENT (if available) TO FILL THIS UP";
+	fs.appendFileSync(filePath, fileContent);
+}
+
+function addFileWithTodaysDate(selectedUri: vscode.Uri): void {
+	const selectedPath: AbsolutePath = selectedUri.path;
+
+	const selected = fs.lstatSync(selectedPath);
+
+	let newFilePath: AbsolutePath;
+	if (selected.isFile()) {
+		newFilePath = createPathInSameDirectoryAs(selectedPath);
+	} else if (selected.isDirectory()) {
+		newFilePath = createPathUnder(selectedPath);
+	} else {
+		const msg = `Path ${selectedPath} is not a file or a directory`;
+		logError(msg)
+		return;
+	}
+
+	try {
+		createFile(newFilePath);
+	} catch (error) {
+		if (error instanceof FileAlreadyExistsError) {
+			logError(`File ${newFilePath} already exists`);
+		} else {
+			console.error(error)
+		}
+	}
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-files-with-date" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('markdown-files-with-date.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from markdown-files-with-date!');
-	});
+	const disposable = vscode.commands.registerCommand(
+		'markdown-files-with-date.addFileWithTodayDate',
+		addFileWithTodaysDate,
+	);
 
 	context.subscriptions.push(disposable);
 }
